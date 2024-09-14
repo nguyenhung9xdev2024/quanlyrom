f414365f66b6f716531972c854e0185f2eaf4418
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-09-14 15:46:06 +0700

    Update

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // Khởi tạo DataTable
+    var table = $('#rom-table').DataTable({
+        "lengthMenu": [ [10, 20, 30], [10, 20, 30] ]
+    });
+
+    // Lấy dữ liệu từ file JSON và thêm vào bảng
+    fetch('rom-data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(rom => {
+                table.row.add([
+                    rom.release_date,
+                    rom.device_name,
+                    rom.android_version,
+                    rom.developer_name,
+                    `<a href="${rom.download_link}" target="_blank">Tải ROM</a>`
+                ]).draw(false);
+            });
+        });
+
+    // Chức năng tìm kiếm đơn giản
+    $('#search-btn').on('click', function() {
+        var keyword = $('#search-box').val();
+        table.search(keyword).draw();
+    });
+});
